f079c8ef82448869b9ec87e001a2f82e53dbb55b
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-10-05 20:48:57 +0800

    Final Navbar

--- src/components/stateless/Navigation/Navigation.js
+++ src/components/stateless/Navigation/Navigation.js
@@ -11,25 +11,28 @@ const Navigation = () => {
             <img alt='uxsoc-logo' src={logo}></img>
           </div>
 
-          {/* About Us */}
-          <div className='navbar-item navbar-about'>
-            <div className='navbar-link'>About Us</div>
+          <div className='navbar-mr'>
+            {/* About Us */}
+            <div className='navbar-item navbar-about'>
+              <div className='navbar-link'>About Us</div>
+            </div>
+
+            {/* Community */}
+            <div className='navbar-item'>
+              <div className='navbar-link'>Community</div>
+            </div>
+
+            {/* Work */}
+            <div className='navbar-item'>
+              <div className='navbar-link'>Work</div>
+            </div>
+
+            {/* Contact Us */}
+            <div className='navbar-item navbar-contact'>
+              <div className='navbar-link'>Contact Us</div>
+            </div>
           </div>
 
-          {/* Community */}
-          <div className='navbar-item navbar-community'>
-            <div className='navbar-link'>Community</div>
-          </div>
-
-          {/* Work */}
-          <div className='navbar-item navbar-work'>
-            <div className='navbar-link'>Work</div>
-          </div>
-
-          {/* Contact Us */}
-          <div className='navbar-item navbar-contact'>
-            <div className='navbar-link'>Contact Us</div>
-          </div>
         </div>
       </nav>
   );
