3fcf226c41e78ec3d0464e4f1aec22bd0ea8b23c
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-10-05 16:58:58 +0800

    Navbar alpha update

--- src/components/stateless/Navigation/Navigation.js
+++ src/components/stateless/Navigation/Navigation.js
@@ -4,34 +4,34 @@ import './style.css';
 
 const Navigation = () => {
   return(
-    <div>
       <nav className='navbar'>
-        {/* UX Society Logo */}
-        <div className='navbar-item'>
-          <img alt='uxsoc-logo' src={logo}></img>
-        </div>
+        <div className='navbar-container'>
+          {/* UX Society Logo */}
+          <div className='navbar-item navbar-logo'>
+            <img alt='uxsoc-logo' src={logo}></img>
+          </div>
 
-        {/* About Us */}
-        <div className='navbar-item'>
-          <div className='navbar-link'>About Us</div>
-        </div>
+          {/* About Us */}
+          <div className='navbar-item navbar-about'>
+            <div className='navbar-link'>About Us</div>
+          </div>
 
-        {/* Community */}
-        <div className='navbar-item'>
-          <div className='navbar-link'>Community</div>
-        </div>
+          {/* Community */}
+          <div className='navbar-item navbar-community'>
+            <div className='navbar-link'>Community</div>
+          </div>
 
-        {/* Work */}
-        <div className='navbar-item'>
-          <div className='navbar-link'>Work</div>
-        </div>
+          {/* Work */}
+          <div className='navbar-item navbar-work'>
+            <div className='navbar-link'>Work</div>
+          </div>
 
-        {/* Contact Us */}
-        <div className='navbar-item'>
-          <div className='navbar-link'>Contact Us</div>
+          {/* Contact Us */}
+          <div className='navbar-item navbar-contact'>
+            <div className='navbar-link'>Contact Us</div>
+          </div>
         </div>
       </nav>
-    </div>
   );
 }
 
