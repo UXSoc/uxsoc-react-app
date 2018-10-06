529c2b0c13094ab699e72e13b68fc13403aef0db
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-10-06 10:38:51 +0800

    80% Done for Home Page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='uxsoc-website'>
         <Home/>
       </div>
     );
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import Navigation from './../../stateless/Navigation/Navigation';
-import HomeLandingSection from './../../stateless/HomeLandingSection/HomeLandingSection';
-import CommunityConsultancy from './../../stateless/HomeCards/HomeCards.js';
+import landingImage from './../../../resources/img/home-landing.svg';
+import blue1 from './../../../resources/img/home-blue1.svg';
+import blue2 from './../../../resources/img/home-blue-2.svg';
+import HomeCards from './../../stateless/HomeCards/HomeCards';
+import './style.css';
 
 class Home extends Component {
   render() {
     return(
       <div>
         <Navigation/>
-        <HomeLandingSection/>
-        <CommunityConsultancy/>
+          <div className='home-landing-container'>
+            <h3 className='north-star-title'>Our North Star</h3>
+            <h1 className='main-title-leader'>We make <br/> design leaders</h1>
+            <button className='journey-button'>Start Your Journey</button>
+            <img className='home-landing-image' src={landingImage} alt='landing'/>
+          </div>
+          <div className='home-blue-section'>
+            <p className='home-blue-text'>User Experience Society aims to guide the design
+              leaders of tomorrow, creating products and <br/> services that will make the world a better place</p>
+            <img src={blue1} alt='blue1' className='blue1'/>
+              <img src={blue2} alt='blue2' className='blue2'/>
+          </div>
+          <div className="HomeCards-container">
+            <HomeCards
+              cardLabel="Community"
+              headerLabel="Geared for Growth"
+              btnLabel="Experience the Mafia"
+              caption="Engage yourself with the community. Discover what you can do and how you can grow with us."/>
+            <HomeCards
+              cardLabel="Consultancy"
+              headerLabel="Crafted for Work"
+              btnLabel="Explore our projects"
+              caption="We help businesses explore improve their products and services through intentional design, and we never fail to deliver."/>
+          </div>
       </div>
     );
   }
