78197fc658a825269e68eeb9591c606aa30c99b4
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-10-07 21:07:57 +0800

    Minor edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/pages/Home/Home';
 import About from './components/pages/About/About';
