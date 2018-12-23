806f7e906fe43c025edcf24702a6f9b6356e08a0
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-12-24 03:05:40 +0800

    Finished About Page

---
+++ src/components/stateless/AboutValues/LeftValues.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LeftValues = props => {
+
+  const ValueSection = styled.div`
+    display: flex;
+    flex-direction: row;
+  `
+
+  const Values = styled.div`
+    margin: auto;
+  `
+
+  const ValueTitle = styled.h3`
+    font-family: Rubik, sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    font-size: 24px;
+    color: #101010;
+  `
+
+  const ValueDetails = styled.p`
+    width: 80%;
+    font-family: Proxima Nova;
+    line-height: 30px;
+    font-size: 18px;
+    color: #101010;
+  `
+
+  return(
+    <ValueSection>
+      <Values>
+        <ValueTitle>{props.title}</ValueTitle>
+        <ValueDetails>{props.details}</ValueDetails>
+      </Values>
+
+      <img src={props.img} alt="Core Value Image"/>
+    </ValueSection>
+  );
+}
+
+export default LeftValues;
